0bc5e2ce02b62d5ac61675c6f7688719233828e7
Ritam Chakraborty <ritam.chakraborty@gmail.com>
2024-10-21 05:35:55 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        instagram: '#e1306c',
+        linkedin: '#0077b5',
+        github: '#6e5494'
+      }
+    },
+  },
+  plugins: [],
+}
+
